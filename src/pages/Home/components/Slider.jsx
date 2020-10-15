f0263808684bf1e746a3b1de58faa044bc70890b
abdulrrehman <abdulrrehman45@gmail.com>
2020-10-15 15:54:30 +0500

    Folder Arrangement

---
+++ src/pages/Home/components/Slider.jsx
@@ -0,0 +1,23 @@
+// import React, { useEffect, useState } from 'react';
+// import { Button } from 'reactstrap';
+// import { GetBannerResults } from '../api/GetRequests';
+
+// function Slider(){
+//     const [ banners, setBanners ] = useState(null);
+
+//     useEffect(() => {
+//         GetBannerResults().then(value => {
+//             setBanners(value);
+//         })
+//     }, []);
+
+//     return(
+//         <div>
+            
+
+//             <h3>Say Hello to AB </h3>
+//         </div>
+//     )
+// }
+
+// export { Slider };
