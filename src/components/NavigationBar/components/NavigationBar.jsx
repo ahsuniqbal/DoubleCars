b7450c648834147cf1194e6931583367535aa136
abdulrrehman <abdulrrehman45@gmail.com>
2020-10-16 21:07:11 +0500

    nav bar css in process

--- src/components/NavigationBar/components/NavigationBar.jsx
+++ src/components/NavigationBar/components/NavigationBar.jsx
@@ -1,41 +1,48 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
+import "../styles/NavigationBar.css"
 
 const NavigationBar = () => {
     return (
         <> 
-            <div className = "container-fluid">
-                <div className = "row">
-                    <div className = "col-10 mx-auto">     
-                        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                            <NavLink className="navbar-brand" to = "/">Double Cars</NavLink>
-                            <button className="navbar-toggler"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target="#navbarSupportedContent"
-                                aria-controls="navbarSupportedContent"
-                                aria-expanded="false"
-                                aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <NavLink className="navbar-brand" to = "/">Double Cars</NavLink>
+                <button className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="navbar-nav ml-auto">
-                                    <li className="nav-item">
-                                        <NavLink className="nav-link" to="/">Home</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink className="nav-link" to="/blogs">Blogs</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink className="nav-link" to="/about">About</NavLink>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/home">New Cars</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/blogs">Used Cars</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/about">Popular Cars</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">Certified Cars</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">Contact Us</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="">Contact Us</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link download-button" to="">Download App</NavLink>
+                        </li>
+                    </ul>   
                 </div>
-            </div>
+            </nav>
         </>
     );
   }
