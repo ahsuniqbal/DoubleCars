3b9d1ef71c1c47167bd807be8b91ecb9e49634ae
abdulrrehman <abdulrrehman45@gmail.com>
2020-10-20 22:28:20 +0500

    Popular section in progress

--- src/pages/Home/components/Home.jsx
+++ src/pages/Home/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import { Slider } from './Slider';
 import Footer from '../../../components/Footer/components/Footer'
+import PopularMake from './PopularMake';
 import SearchBox from './SearchBox';
 const Home = () => {
     return(
         <div>
-        <SearchBox/>
+        <PopularMake/>
         <Footer/>
 
     </div>
