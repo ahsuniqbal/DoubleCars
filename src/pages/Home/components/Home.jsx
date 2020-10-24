e7c4d7d3473dd5aaf530d9cfa0667ad6cd825435
abdulrrehman <abdulrrehman45@gmail.com>
2020-10-24 15:32:54 +0500

    IMAGES WORK TEST

--- src/pages/Home/components/Home.jsx
+++ src/pages/Home/components/Home.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
-import Cover from '../../../assets/landingPageCover.png'
+import Cover from '../../../assets/DummyCarCard.png'
 import Footer from '../../../components/Footer/components/Footer'
 import { NavLink } from "react-router-dom";
 import PopularMake from './PopularMake';
 import SearchBox from './SearchBox';
+import {Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle, Button, Row, Col} from 'reactstrap';
+import Header from './Header';
 const Home = () => {
     return(
         <div>
-            
-                <div className = "container-fluid">
-                    <div className = "row">
-                        <div className = "col-12">
-                            <div className = "row">
-                                <div className = "col-md-5 pt-lg-0 order-2 order-lg-1 left-cover-section">
-                                    <h1 className = "cover-left-heading">
-                                        Find the most desireable car
-                                    </h1>
-                                    <h5 className = "my-3">
-                                        Buy and sell cars with ease using our app!
-                                    </h5>
-                                    <div className = "mt-3">
-                                    <NavLink className="nav-link download-button-cover" to="">Download App</NavLink>
-                                    </div>
-                                </div>
-
-                                <div className = "col-lg-7  order-1 order-lg-2">
-                                    <img src = {Cover} alt = "Cover image" className = "img-fluid"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            
-
-
-        <PopularMake/>
-        <Footer/>
-
-    </div>
+            <Row>
+                <Col md = "4" lg="3">
+                    <Card>
+                        <CardImg src={Cover} alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle>Card title</CardTitle>
+                                <CardSubtitle>Card subtitle</CardSubtitle>
+                                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                <Button>Button</Button>
+                            </CardBody>
+                    </Card>
+                </Col>
+                <Col md = "4" lg="3">
+                    <Card>
+                        <CardImg src={Cover} alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle>Card title</CardTitle>
+                                <CardSubtitle>Card subtitle</CardSubtitle>
+                                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                <Button>Button</Button>
+                            </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+<Header/>
+            <PopularMake/>
+      
+        </div>
     )
 }
 
--- src/pages/Home/components/PopularMake.jsx
+++ src/pages/Home/components/PopularMake.jsx
@@ -9,129 +9,75 @@ import Mitsubishi from '../../../assets/Mitsubishi.png'
 import Suzuki from '../../../assets/Suzuki.png'
 import Tesla from '../../../assets/Tesla.png'
 import Toyota from '../../../assets/Toyota.png'
-import {Row, Col, Label} from 'reactstrap'
+import {Row, Col, Label, Button} from 'reactstrap'
 import { Link } from 'react-router-dom';
+import '../styles/PopularMake.css'
 
 
 
-// function DrawRows(){
-//     var table = [];
-//     var ourLeagueImages = [Suzuki, Mitsubishi, Mercedes, Lexus, Honda, Ford, BMW, Audi];
-//     for(let i = 0; i < ourLeagueImages.length; i++){
-//         table.push(
-
-//             <Row className="mx-0">
-//                 {
-//                     this.DrawCols(ourLeagueImages, i)
-//                 }
-//                 <Col xs="12" className="my-2">
-//                     <hr />
-//                 </Col>
-//             </Row>
-//         )
-//         i += 3;
-//     }
-//     return table;
-// }
-// function DrawCols(ourLeagueImages, index){
-//     var table = [];
-//     for (let i = index; i < index + 4; i++) {
-//         table.push(
-//             <Col xs="12" md="3" className="text-center my-2">
-//                 <img data-aos="flip-up" src={ourLeagueImages[i]} alt="Our league person" className="img-fluid" />
-//             </Col>
-//         )
-//     }
-//     return table;
-// }
-
-
-// const Home = () => {
-//     return(
-//         <div className="our-league-section">
-//                 <Row className="mx-0">
-//                     <Col xs="12">
-//                         <h1 className="text-center my-4">Our League</h1>
-//                     </Col>
-//                 </Row>
-
-//                 {
-//                     this.DrawRows()
-//                 }
-
-//                 <Row className="mx-0">
-//                     <Col xs="12" className="text-center mt-3 mb-5">
-//                         <h3 className="mb-4">Quite Simply: It really works. So why wait?</h3>
-//                         <Link to="/signup"><Button className="white-btn" color="light">Join Now</Button></Link>
-//                     </Col>
-//                 </Row>
-//             </div>
-//     )
-// }
-
-// export default Home;
-
-
-
-
-
-
-
-
-
-    class PopularMake extends Component {
-        DrawRows(){
-            var table = [];
-            var popularMakesImages = [Suzuki, Mitsubishi, Mercedes, Lexus, Honda, Ford, BMW, Audi];
-            for(let i = 0; i < popularMakesImages.length; i++){
-                table.push(
+const DrawRows = () => {
+    var table = [];
     
-                    <Row className="mx-0">
-                        {
-                            this.DrawCols(popularMakesImages, i)
-                        }
-                        <Col xs="12" className="my-2">
-                          
-                        </Col>
-                    </Row>
-                )
-                i += 3;
-            }
-            return table;
-        }
-    
-        DrawCols(popularMakesImages, index){
-            var table = [];
-            for (let i = index; i < index + 4; i++) {
-                table.push(
-                    <Col xs="12" md="2" className="text-center my-2">
-                        <img data-aos="flip-up" src={popularMakesImages[i]} alt="Our league person" className="img-fluid" />
-                        
-                    </Col>
-                    
-                )
-            }
-            return table;
-        }
-    
-        render(){
-            return(
-                <div className="our-league-section">
-                    <Row className="mx-0">
-                        <Col xs="12">
-                            <h5 className="my-4">Popular Make</h5>
-                        </Col>
-                    </Row>
-    
-                    {
-                        this.DrawRows()
-                        
-                    }
-    
-                    
-                </div>
-            );
-        }
+    for(let i = 0; i < 2; i++){
+        table.push(
+
+            <Row className="mx-0">
+                {
+                    DrawCols()
+                }
+                
+            </Row>
+        )
+        
     }
-    
-    export default PopularMake;
+    return table;
+}
+const DrawCols = () => {
+    var table = [];
+    var ourLeagueImages = [Suzuki, Mitsubishi, Mercedes, Lexus, Honda, Ford, BMW, Audi, Tesla, Toyota];
+    for (let i = 0; i <  6; i++) {
+        table.push(
+
+                <li>
+                    <img data-aos="flip-up" src={ourLeagueImages[i]} alt="Our league person" className="img-fluid" />
+                </li>
+            // <Col md = "2" className="text-center my-2 col-md-offset-2">
+            //     <img data-aos="flip-up" src={ourLeagueImages[i]} alt="Our league person" className="img-fluid" />
+            // </Col>
+        )
+    }
+    return table;
+}
+
+
+const Home = () => {
+    return(
+        <div className="container-fluid our-league-section">
+                <Row className="mx-0">
+                    <Col xs="12">
+                        <h1 className="text-center my-4">Our League</h1>
+                    </Col>
+                </Row>
+                
+
+                    
+                <ul className = "client-list">
+                        
+                
+                {
+                    
+                    DrawCols()
+                }
+                </ul>
+                
+                
+            </div>
+    )
+}
+
+export default Home;
+
+
+
+
+
