a85eaf5522199f8d9e22fe799a4921475595086a
abdulrrehman <abdulrrehman45@gmail.com>
2020-10-27 01:52:13 +0500

    vbc

--- src/pages/ProductResults/components/ProductResults.jsx
+++ src/pages/ProductResults/components/ProductResults.jsx
@@ -7,10 +7,20 @@ const ProductResults = () => {
         <Container>
             <Row>
                 <Col className = "" md = "6" >
-                    <NavLink className="" to="/products">Back to search results</NavLink>                
+                    <NavLink className="back-button" to="/products">Back to search results</NavLink>                
                 </Col>
                 <Col className = "" md = "6" >
                     <NavLink className="float-right report-button" to="/products">Report this car</NavLink>                
+                </Col>
+            </Row>
+
+            <Row>
+                <Col md = "7">
+                    <h3>Image gallery here</h3>
+                </Col>
+                <Col md = "5">
+                    <h6>Are you interested in this car?</h6>
+                    <h6>Are you interested in this car?</h6>
                 </Col>
             </Row>
         </Container>
