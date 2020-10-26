7169e29984888c11d50b0a2ca1f74b6bb9e304a1
abdulrrehman <abdulrrehman45@gmail.com>
2020-10-27 00:37:26 +0500

    product results routes complete

--- src/navigation/Routes.jsx
+++ src/navigation/Routes.jsx
@@ -6,6 +6,7 @@ const Blogs = React.lazy(() => import('../pages/Blogs'));
 const Profile = React.lazy(() => import('../pages/Profile'));
 const Login = React.lazy(() => import('../pages/Authentication/Login'));
 const Products = React.lazy(() => import('../pages/Products'));
+const productResults = React.lazy(() => import ('../pages/ProductResults'))
 
 const routes = [
     { path: '/', exact: true, name: 'Home', public: true, restricted: false, component: Home },
@@ -14,6 +15,7 @@ const routes = [
     { path: '/profile', exact: true, name: 'Profile', public: false, restricted: false, component: Profile },
     { path: '/login', exact: true, name: 'Login', public: true, restricted: true, component: Login },
     { path: '/products', exact: true, name: 'Products', public: true, restricted: true, component: Products },
+    { path: '/productresults', exact: true, name: 'ProductResults', public: true, restricted: true, component: productResults },
 ];
 
 export default routes;
---
+++ src/pages/ProductResults/components/ProductResults.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductResults = () => {
+    return(
+        <div>
+        <h3>This is a products results page </h3>
+    </div>
+    )
+}
+
+export default ProductResults;
