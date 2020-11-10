66557d62f988eb1a78c12721d998dbd236988b3e
abdulrrehman <abdulrrehman45@gmail.com>
2020-11-10 13:34:29 +0500

    working on landing page.

--- src/components/CarouselCard/components/CarouselCard.jsx
+++ src/components/CarouselCard/components/CarouselCard.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Row, Col, Label, Button, Badge } from 'reactstrap';
-import CompanyLogo from '../../../assets/company-logo.png';
-
+import CompanyLogo from '../../../assets/DummyCarCard.png';
 import '../styles/CarouselCard.css';
 import { Link } from 'react-router-dom';
 
 const CarouselCard = (props) => {
   return (
-    <Card className="product-card mb-3">
-            <div className="product-img">
+    <Card className="product-card mb-3 ml-3">
+            <div className="">
                 <CardImg src = {CompanyLogo} />
             </div>
         <CardBody>
                 <Row>
                     <Col xs="9">
-                        <CardTitle >Car title</CardTitle>
-                        <CardSubtitle>Money</CardSubtitle>
-                        <CardText>proce</CardText>
+                        <CardTitle className = "carousel-car-name" >2019 Mercedes Benz Hybrid</CardTitle>
+                        <CardSubtitle className = "carousel-car-info">19,850 mileage - california </CardSubtitle>
+                        <CardText className = "carousel-car-price">$32,500</CardText>
                     </Col>
                     
                 </Row>
--- src/pages/Home/components/HomeImageSlider.jsx
+++ src/pages/Home/components/HomeImageSlider.jsx
@@ -8,6 +8,8 @@ import DummyCarCard from '../../../assets/DummyCarCard.png'
 import DummyCarCard2 from '../../../assets/DummyCarCard2.png'
 import DummyCarCard3 from '../../../assets/DummyCarCard3.png'
 import CarouselCard from '../../../components/CarouselCard/components/CarouselCard';
+import { Col, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
 // import {Button, Label, Row, Col, Input} from 'reactstrap'
 const HomeImageSlider = () => {
 
@@ -15,43 +17,38 @@ const HomeImageSlider = () => {
     dot:true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     cssEase: "linear",
     }
 
     return(
         <div>
+            <Row className = "mt-5">
+                <Col md = "6" xs = "12">
+                    <h2 className = "recommended-cars-head">Recommended Cars</h2>
+                </Col>
+
+                <Col md = "6" xs = "12" className = "text-right">
+                    <Link>View All</Link>
+                </Col>
+            </Row>
+        
+
               <Slider {...settings}>
               <CarouselCard/>
-                  <div className = "card-wrapper">
-                    <div className = "card">
-                        <div className = "card-image">
-                            <img src = {DummyCarCard} alt = "car image"/>
-                        </div>
-                    </div>
-                  </div>
-                  <div className = "card-wrapper">
-                    <div className = "card">
-                        <div className = "card-image">
-                            <img src = {DummyCarCard} alt = "car image"/>
-                        </div>
-                    </div>
-                  </div>
-                  <div className = "card-wrapper">
-                    <div className = "card">
-                        <div className = "card-image">
-                            <img src = {DummyCarCard} alt = "car image"/>
-                        </div>
-                    </div>
-                  </div>
-                  <div className = "card-wrapper">
-                    <div className = "card">
-                        <div className = "card-image">
-                            <img src = {DummyCarCard} alt = "car image"/>
-                        </div>
-                    </div>
-                  </div>
+              <CarouselCard/>
+              <CarouselCard/>
+              <CarouselCard/>
+              <CarouselCard/>
+              <CarouselCard/>
+              <CarouselCard/>
+              <CarouselCard/>
+              <CarouselCard/>
+                  
+                 
+                  
+                  
                   
                   </Slider> 
         </div>
