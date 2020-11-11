563505c01699e1fe707221d21cd7637200395ec8
abdulrrehman <abdulrrehman45@gmail.com>
2020-11-11 19:37:21 +0500

    sfasa

--- src/pages/Home/components/TrendingBodyTypes/TrendingBody1.jsx
+++ src/pages/Home/components/TrendingBodyTypes/TrendingBody1.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
-
-
-
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import DummyCarCard from '../../../../assets/DummyCarCard.png'
+import ProductCard from '../../../../components/ProductCard/components/ProductCard';
 const TrendingBody1 = (props) => {
+    let settings = {
+        dot:true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        cssEase: "linear",
+        }
     return(
         <div>    
-            This is a body 1
+            <Slider {...settings}>
+                <ProductCard
+                    productImg = {DummyCarCard}
+                    
+                />
+                <ProductCard
+                    productImg = {DummyCarCard}
+                />
+                <ProductCard
+                    productImg = {DummyCarCard}
+                />
+                <ProductCard
+                    productImg = {DummyCarCard}
+                />
+                <ProductCard
+                    productImg = {DummyCarCard}
+
+                />
+                <ProductCard
+                    productImg = {DummyCarCard}
+                />
+            </Slider> 
         </div>    
     );
 };
