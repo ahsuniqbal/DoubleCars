f77507c7208471c045e7868256c29e36f5e57397
abdulrrehman <abdulrrehman45@gmail.com>
2020-11-11 18:43:08 +0500

    tab bars

--- src/pages/Home/components/TrendingBodyTypes/TrendingBody4.jsx
+++ src/pages/Home/components/TrendingBodyTypes/TrendingBody4.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const TrendingBody4 = () => {
     return(
         <div>    
-            This is a body 1
+            This is a body 4
         </div>    
     );
 };
--- src/pages/Home/components/TrendingBodyTypes/TrendingBodyTypes.jsx
+++ src/pages/Home/components/TrendingBodyTypes/TrendingBodyTypes.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Row, Col, TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 import TrendingBody1 from './TrendingBody1';
 import TrendingBody2 from './TrendingBody2';
@@ -6,76 +6,179 @@ import TrendingBody3 from './TrendingBody3';
 import TrendingBody4 from './TrendingBody4';
 import TrendingBody5 from './TrendingBody5';
 import TrendingBody6 from './TrendingBody6';
-
-// import '../../views/Settings.css'
 import '../../styles/TrendingBodyTypes.css'
 import classnames from 'classnames';
 
-const TrendingBodyTypes = () =>{
+// constructor(props){
+//     super(props);
+//     this.state = {
+//         activeTab: '1',
+//     }
+// }
+
+// toggleTabs = (e, tabIndex) =>{
+//     e.preventDefault();
+//     if(this.state.activeTab !== tabIndex){
+//         this.setState({activeTab: tabIndex});
+//     }
+// }
+
+
+
+// const TrendingBodyTypes = () =>{
         
+//         return(
+//             <div>
+//                 <Row>
+//                     <Col md = "12" xs = "12" className = "text-center mt-5">
+//                         <h2 className = "trending-body-head">Trending Body Type in 2020</h2>
+//                     </Col>
+//                     <Col xs="12">
+//                         <Nav>
+//                             <NavItem>
+//                                 <NavLink>
+//                                    SUV
+//                                 </NavLink>
+//                             </NavItem>
+
+//                             <NavItem>
+//                                 <NavLink >
+//                                     Sedan
+//                                 </NavLink>
+//                             </NavItem>
+
+//                             <NavItem>
+//                                 <NavLink >
+//                                     Hatchback
+//                                 </NavLink>
+//                             </NavItem>
+
+//                             <NavItem>
+//                                 <NavLink >
+//                                     Crossover
+//                                 </NavLink>
+//                             </NavItem>
+
+//                             <NavItem>
+//                                 <NavLink >
+//                                     Coup
+//                                 </NavLink>
+//                             </NavItem>
+
+//                             <NavItem>
+//                                 <NavLink >
+//                                     Covertable
+//                                 </NavLink>
+//                             </NavItem>
+//                         </Nav>
+
+//                         <TabContent >
+//                             <TabPane tabId="1">
+//                                 <TrendingBody1/>
+                                
+//                             </TabPane>
+                            
+//                             <TabPane tabId="2">
+//                                 <TrendingBody2/>
+//                             </TabPane>
+
+//                             <TabPane tabId="3">
+//                                 <TrendingBody3/>
+//                             </TabPane>
+
+//                             <TabPane tabId="4">
+//                                 <TrendingBody4/>
+//                             </TabPane>
+
+//                             <TabPane tabId="5">
+//                                 <TrendingBody5/>
+//                             </TabPane>
+//                             <TabPane tabId="6">
+//                                 <TrendingBody6/>
+//                             </TabPane>
+//                         </TabContent>
+//                     </Col>
+//                 </Row>
+//             </div>
+//         );
+    
+// }
+// export default TrendingBodyTypes;
+
+
+class TrendingBodyTypes extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            activeTab: '1',
+        }
+    }
+
+    toggleTabs = (e, tabIndex) =>{
+        e.preventDefault();
+        if(this.state.activeTab !== tabIndex){
+            this.setState({activeTab: tabIndex});
+        }
+    }
+
+    
+
+
+    render(){
         return(
             <div>
                 <Row>
-                    <Col md = "12" xs = "12" className = "text-center mt-5">
-                        <h2 className = "trending-body-head">Trending Body Type in 2020</h2>
-                    </Col>
                     <Col xs="12">
-                        <Nav>
+                        <Nav tabs>
                             <NavItem>
-                                <NavLink>
-                                   SUV
+                                <NavLink className={classnames({ active: this.state.activeTab === '1' })} onClick={(e) => this.toggleTabs(e, '1')}>
+                                    SUV
                                 </NavLink>
                             </NavItem>
 
                             <NavItem>
-                                <NavLink >
+                                <NavLink className={classnames({ active: this.state.activeTab === '2' })} onClick={(e) => this.toggleTabs(e, '2')}>
                                     Sedan
                                 </NavLink>
                             </NavItem>
 
                             <NavItem>
-                                <NavLink >
+                                <NavLink className={classnames({ active: this.state.activeTab === '3' })} onClick={(e) => this.toggleTabs(e, '3')}>
                                     Hatchback
                                 </NavLink>
                             </NavItem>
 
                             <NavItem>
-                                <NavLink >
+                                <NavLink className={classnames({ active: this.state.activeTab === '4' })} onClick={(e) => this.toggleTabs(e, '4')}>
                                     Crossover
                                 </NavLink>
                             </NavItem>
-
                             <NavItem>
-                                <NavLink >
+                                <NavLink className={classnames({ active: this.state.activeTab === '5' })} onClick={(e) => this.toggleTabs(e, '5')}>
                                     Coup
                                 </NavLink>
                             </NavItem>
-
                             <NavItem>
-                                <NavLink >
+                                <NavLink className={classnames({ active: this.state.activeTab === '6' })} onClick={(e) => this.toggleTabs(e, '6')}>
                                     Covertable
                                 </NavLink>
                             </NavItem>
                         </Nav>
 
-                        <TabContent >
+                        <TabContent activeTab={this.state.activeTab}>
                             <TabPane tabId="1">
-                                <TrendingBody1/>
-                                
+                               <TrendingBody1/>
                             </TabPane>
                             
                             <TabPane tabId="2">
                                 <TrendingBody2/>
                             </TabPane>
-
                             <TabPane tabId="3">
                                 <TrendingBody3/>
                             </TabPane>
-
                             <TabPane tabId="4">
                                 <TrendingBody4/>
                             </TabPane>
-
                             <TabPane tabId="5">
                                 <TrendingBody5/>
                             </TabPane>
@@ -87,6 +190,6 @@ const TrendingBodyTypes = () =>{
                 </Row>
             </div>
         );
-    
+    }
 }
 export default TrendingBodyTypes;
