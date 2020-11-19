09498abd3dc2e5a5f8b9db8cb7a93306d69a731d
abdulrrehman <abdulrrehman45@gmail.com>
2020-11-19 18:40:27 +0500

    sa

--- src/pages/DealerProfile/components/SellerDetails.jsx
+++ src/pages/DealerProfile/components/SellerDetails.jsx
@@ -6,77 +6,13 @@ import { Phone, Mail } from 'react-feather';
 
 const SellerDetails = (props) => {
     return(
-        //     <Card className = "seller-card">
-        //         <CardBody>
-        //             <Row>
-        //                 <Col md = "8">
-        //                     <Row>
-        //                         <Col md = "2">
-        //                             {
-        //                                 props.profilePic ?
-        //                                 <CardImg className = "Dealer-Profile-image" src={props.profilePic} alt="Company logo" />
-        //                                 :
-        //                                 null
-        //                             }
-                                    
-        //                         </Col>
-        //                         <Col md = "10">
-        //                             <Label className = "dealer-name">{props.fullName}</Label> <br/>
-        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-2" />
-        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-2" />
-        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-2" />
-        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#DBDBDB" size="1x" className="mr-2" />
-        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#DBDBDB" size="1x" className="mr-2" />
-        //                             <Label style= {{color: "#FFC061"}}>4.2</Label>
-        //                         </Col>
-        //                     </Row>
-        //                     <Row className = "mt-3">
-        //                         <Col md = "7">
-        //                             <h6 className = "about-seller-head">About seller</h6>
-        //                             {
-        //                                 props.aboutMe ? 
-        //                                 <p className = "seller-details-p">{props.aboutMe}</p>
-        //                                 :
-        //                                 <p className = "seller-details-p">This seller has provided no information</p>
-        //                             }
-        //                         </Col>
-        //                         <Col md = "5">
-        //                             <h6 className = "contact-details">Contact Details</h6>
-                                    
-        //                             <p className = "contact-details-p"><Phone color="#1C67CE" size={20} className = "mr-2"/>{props.phNum}</p>
-        //                             <p className = "contact-details-p"><Mail color="#1C67CE" size={20} className = "mr-2"/>{props.email}</p>
-        //                         </Col>
-        //                     </Row>
-        //                 </Col>
-        //                 <Col className = "ml-4">
-        //                     <div className = "vertical-line"></div>
-        //                 </Col>
-                        
-                        
-                        
-        //                 <Col md = "3" xs = "12" sm = "12" className = "text-center">
-                            
-        //                     <h1 className = "rate-label">4.45</h1>
-        //                     <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-1 font-rate-stars" />
-        //                     <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-1 font-rate-stars" />
-        //                     <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-1 font-rate-stars" />
-        //                     <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-1 font-rate-stars" />
-        //                     <FontAwesomeIcon icon={["fas", "star"]} color="#DBDBDB" size="1x" className="mr-1 font-rate-stars" /> <br/>
-        //                     <Label className = "reviews-label"> 154 reviews</Label> <br/>
-        //                     <Button size = "lg" block className = "read-reviews-button primary mt-4 float-left">Read reviews</Button> 
-                           
-                            
-        //                 </Col>
-        //                 </Row>  
-        //     </CardBody>
-        // </Card>
-
-        <Card className = "seller-card">
+        // Public seller card starts here
+            <Card className = "seller-card">
                 <CardBody>
                     <Row>
-                        <Col md = "12">
+                        <Col md = "8">
                             <Row>
-                                <Col md = "2" xs = "12">
+                                <Col md = "2">
                                     {
                                         props.profilePic ?
                                         <CardImg className = "Dealer-Profile-image" src={props.profilePic} alt="Company logo" />
@@ -85,7 +21,7 @@ const SellerDetails = (props) => {
                                     }
                                     
                                 </Col>
-                                <Col md = "10" xs = "12">
+                                <Col md = "10">
                                     <Label className = "dealer-name">{props.fullName}</Label> <br/>
                                     <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-2" />
                                     <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-2" />
@@ -96,16 +32,16 @@ const SellerDetails = (props) => {
                                 </Col>
                             </Row>
                             <Row className = "mt-3">
-                                <Col md = "8">
+                                <Col md = "7">
                                     <h6 className = "about-seller-head">About seller</h6>
                                     {
                                         props.aboutMe ? 
                                         <p className = "seller-details-p">{props.aboutMe}</p>
                                         :
-                                        <p className = "seller-details-p"> This seller has provided no information</p>
+                                        <p className = "seller-details-p">This seller has provided no information</p>
                                     }
                                 </Col>
-                                <Col md = "4">
+                                <Col md = "5">
                                     <h6 className = "contact-details">Contact Details</h6>
                                     
                                     <p className = "contact-details-p"><Phone color="#1C67CE" size={20} className = "mr-2"/>{props.phNum}</p>
@@ -113,10 +49,75 @@ const SellerDetails = (props) => {
                                 </Col>
                             </Row>
                         </Col>
+                        <Col className = "ml-4">
+                            <div className = "vertical-line"></div>
+                        </Col>
                         
+                        
+                        
+                        <Col md = "3" xs = "12" sm = "12" className = "text-center">
+                            
+                            <h1 className = "rate-label">4.45</h1>
+                            <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-1 font-rate-stars" />
+                            <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-1 font-rate-stars" />
+                            <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-1 font-rate-stars" />
+                            <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-1 font-rate-stars" />
+                            <FontAwesomeIcon icon={["fas", "star"]} color="#DBDBDB" size="1x" className="mr-1 font-rate-stars" /> <br/>
+                            <Label className = "reviews-label"> 154 reviews</Label> <br/>
+                            <Button size = "lg" block className = "read-reviews-button primary mt-4 float-left">Read reviews</Button> 
+                           
+                            
+                        </Col>
                         </Row>  
             </CardBody>
         </Card>
+        // Private seller card starts here
+        // <Card className = "seller-card">
+        //         <CardBody>
+        //             <Row>
+        //                 <Col md = "12">
+        //                     <Row>
+        //                         <Col md = "2" xs = "12">
+        //                             {
+        //                                 props.profilePic ?
+        //                                 <CardImg className = "Dealer-Profile-image" src={props.profilePic} alt="Company logo" />
+        //                                 :
+        //                                 null
+        //                             }
+                                    
+        //                         </Col>
+        //                         <Col md = "10" xs = "12">
+        //                             <Label className = "dealer-name">{props.fullName}</Label> <br/>
+        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-2" />
+        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-2" />
+        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#FFBB54" size="1x" className="mr-2" />
+        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#DBDBDB" size="1x" className="mr-2" />
+        //                             <FontAwesomeIcon icon={["fas", "star"]} color="#DBDBDB" size="1x" className="mr-2" />
+        //                             <Label style= {{color: "#FFC061"}}>4.2</Label>
+        //                         </Col>
+        //                     </Row>
+        //                     <Row className = "mt-3">
+        //                         <Col md = "8">
+        //                             <h6 className = "about-seller-head">About seller</h6>
+        //                             {
+        //                                 props.aboutMe ? 
+        //                                 <p className = "seller-details-p">{props.aboutMe}</p>
+        //                                 :
+        //                                 <p className = "seller-details-p"> This seller has provided no information</p>
+        //                             }
+        //                         </Col>
+        //                         <Col md = "4">
+        //                             <h6 className = "contact-details">Contact Details</h6>
+                                    
+        //                             <p className = "contact-details-p"><Phone color="#1C67CE" size={20} className = "mr-2"/>{props.phNum}</p>
+        //                             <p className = "contact-details-p"><Mail color="#1C67CE" size={20} className = "mr-2"/>{props.email}</p>
+        //                         </Col>
+        //                     </Row>
+        //                 </Col>
+                        
+        //                 </Row>  
+        //     </CardBody>
+        // </Card>
     )
 }
 
