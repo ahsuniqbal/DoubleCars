967b9c4508688e0c39691be1f6433e91beedca1a
ahsuniqbal <ahsuniqbal1505@gmail.com>
2020-12-03 01:29:35 +0500

    Basic filters UI done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Switch, HashRouter as Router, Route } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faStar, faMapPin, faPhone, faSearch, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faStar, faMapPin, faPhone, faSearch, faEnvelope, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import { faBookmark } from '@fortawesome/free-regular-svg-icons';
 import logo from './assets/DCLogo.jpg';
 
 
-library.add(faUser, faStar, faBookmark, faMapPin, faPhone, faSearch, faEnvelope);
+library.add(faUser, faStar, faBookmark, faMapPin, faPhone, faSearch, faEnvelope, faPlus, faMinus);
 
 //fallback loading
 const loading = () => <div className="preloader">
