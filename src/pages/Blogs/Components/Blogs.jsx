ba5badb391fd0b6016e5357d6cd42daa9ae167a7
abdulrrehman <abdulrrehman45@gmail.com>
2020-12-04 01:16:52 +0500

    dwdd

--- src/pages/Blogs/Components/Blogs.jsx
+++ src/pages/Blogs/Components/Blogs.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Row, Col, Container, Label } from 'reactstrap';
 import '../styles/Blogs.css'
+import BlogsDemoImage1 from '../../../assets/BlogsDemoImage1.png' 
 const Blogs = () => {
     return(
-        <body className = "">
+        <body className = "blogs-body">
             <Container>
                 <Row>
-                    <Col xs = "12" md = "6" sm = "12" className = "contactus-column">
-                       
+                    <Col xs = "12" md = "12" sm = "12" className = "blogs-column">
+                       <Label className = "blog-read-label">23 December  •  2 min read </Label> <br/>
+                       <h3 className = "blogs-heading">The next generation car is here!</h3>
+                        <img src = {BlogsDemoImage1} className = "img-fluid" width= "100%"/>
                     </Col>
                 </Row>
             </Container>
