47983671974778a50f5d32f404eac3d6151e0e3e
abdulrrehman <abdulrrehman45@gmail.com>
2020-12-09 00:53:57 +0500

    sas

--- src/pages/Blogs/Components/Blogs.jsx
+++ src/pages/Blogs/Components/Blogs.jsx
@@ -3,6 +3,7 @@ import { Button, Row, Col, Container, Label} from 'reactstrap';
 import '../styles/Blogs.css'
 import BlogsDemoImage1 from '../../../assets/BlogsDemoImage1.png' 
 import RelatedStoriesCell from './RelatedStoriesCell';
+import SimilarCarCell from './SimilarCarCell';
 const Blogs = () => {
     return(
         <body className = "blogs-body">
@@ -16,12 +17,22 @@ const Blogs = () => {
                 </Row>
                 <Row>
                     <Col md = "8" sm = "12" xs = "12">
+                        <p className = "blog-content mt-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci lorem, mattis at porta vitae, accumsan vel libero. In sit amet magna nunc. Curabitur at purus iaculis, rutrum neque sit amet, ornare lorem. Donec est metus, sagittis a nunc suscipit, varius facilisis est. In hac habitasse platea dictumst. Nulla dictum metus id risus porta, sed venenatis libero egestas. Donec laoreet ac ex eget rhoncus. Proin vel diam luctus, finibus mi ac, condimentum nunc. Sed interdum lacinia turpis, et tempus neque laoreet a. Nulla nec aliquam velit. Cras sit amet leo a elit egestas congue quis sit amet lacus. </p>
+                        <h4 className = "blog-sub-heading mb-4">What abour price?</h4>
                         <p className = "blog-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci lorem, mattis at porta vitae, accumsan vel libero. In sit amet magna nunc. Curabitur at purus iaculis, rutrum neque sit amet, ornare lorem. Donec est metus, sagittis a nunc suscipit, varius facilisis est. In hac habitasse platea dictumst. Nulla dictum metus id risus porta, sed venenatis libero egestas. Donec laoreet ac ex eget rhoncus. Proin vel diam luctus, finibus mi ac, condimentum nunc. Sed interdum lacinia turpis, et tempus neque laoreet a. Nulla nec aliquam velit. Cras sit amet leo a elit egestas congue quis sit amet lacus. </p>
+                        <img src = {BlogsDemoImage1} className = "img-fluid"/>
+                        <p className = "blog-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci lorem, mattis at porta vitae, accumsan vel libero. In sit amet magna nunc. Curabitur at purus iaculis, rutrum neque sit amet, ornare lorem. Donec est metus, sagittis a nunc suscipit, varius facilisis est. In hac habitasse platea dictumst. Nulla dictum metus id risus porta, sed venenatis libero egestas. Donec laoreet ac ex eget rhoncus. Proin vel diam luctus, finibus mi ac, condimentum nunc. Sed interdum lacinia turpis, et tempus neque laoreet a. Nulla nec aliquam velit. Cras sit amet leo a elit egestas congue quis sit amet lacus. </p>
+                        
                     </Col>
 
                     <Col md = "4" sm = "12" xs = "12">
-                        <Label className = "related-stories-heading">Related Stories</Label>
+                        <Label className = "cell-heading">Related Stories</Label>
                         <RelatedStoriesCell/>
+                        <RelatedStoriesCell/>
+                        
+                        <Label className = "cell-heading">Similar Cars for Sell</Label>
+                        <SimilarCarCell/>
+                        <SimilarCarCell/>
                     </Col>
                 </Row>
             </Container>
