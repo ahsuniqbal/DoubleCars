2587575f25bbee0db3fa5998dc8bd9b763f4fb81
abdulrrehman <abdulrrehman45@gmail.com>
2020-12-19 17:07:57 +0500

    login

--- src/pages/Authentication/Login/components/Login.jsx
+++ src/pages/Authentication/Login/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 import { login } from '../../../../config/LoginAuth';
-
+import '../styles/Login.css'
+import {Row, Col, Input, Button, NavLink, Container, Label} from 'reactstrap'
 
 const Login = (props) => {
     const handleLogin = () => {
@@ -10,8 +10,18 @@ const Login = (props) => {
     }
     return(
         <div>
-            Login Page
-            <Button onClick={() => handleLogin()}>Click here to login</Button>
+                <Container fluid = {true}>
+                    <Row>
+                        <Col xs = "12" md = "7" sm = "12" className = "login-left-image"></Col>
+                        <Col xs = "12" md = "5" sm = "12" className = "right-side-column text-center">
+                            <h2 className = "login-now-head">Login now</h2>
+                            <Label className = "login-label">Si sine causa, nollem me ab eo ortum, tam egregios viros censes tantas.</Label>
+                            <Input type = "email" className = "login-email" placeholder = "Your Email Address"></Input>
+                            <Input type = "password" className = "login-password" placeholder = "Your Password"></Input>
+                        </Col>
+                    </Row>
+                </Container>
+           
         </div>
     );
 }
