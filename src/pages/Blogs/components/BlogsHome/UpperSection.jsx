eaa8aad4f2e611b8f14ff0e5a555b994929eb447
abdulrrehman <abdulrrehman45@gmail.com>
2020-12-23 14:03:21 +0500

    check

--- src/pages/Blogs/components/BlogsHome/UpperSection.jsx
+++ src/pages/Blogs/components/BlogsHome/UpperSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Label } from 'reactstrap';
 import '../../styles/UpperSection.css'
 
 const UpperSection = () => {
@@ -39,15 +39,39 @@ const UpperSection = () => {
 
     return(
         <Row>
-            <Col md = "6" xs = "12" style = {BlogpageImage1}></Col>
-            <Col md = "3" xs = "12" style = {BlogpageImage2}></Col>
+            <Col md = "6" xs = "12" style = {BlogpageImage1}>
+              <div className = "upper-section-column">
+                <Label className = "type-label">New Cars</Label>
+                <h2 className = "car-name">Video: BMW M2 CS is part of Autocar’s 2020 Best Sports Cars</h2>
+                <Label className = "date-label">Dec 15, 2020</Label>
+              </div>
+              </Col>
+            <Col md = "3" xs = "12" style = {BlogpageImage2}>
+            <div className = "">
+                <Label className = "type-label">New Cars</Label>
+                <h2 className = "car-name">Video: BMW M2 CS is part of Autocar’s 2020 Best Sports Cars</h2>
+                <Label className = "date-label">Dec 15, 2020</Label>
+              </div>
+            </Col>
             <Col md = "3" xs = "12">
                 <Row>
-                    <Col md = "12" xs = "12" style = {BlogpageImage3}></Col>
+                    <Col md = "12" xs = "12" style = {BlogpageImage3}>
+                    <div className = "">
+                <Label className = "type-label">New Cars</Label>
+                <h2 className = "car-name">Video: BMW M2 CS is part of Autocar’s 2020 Best Sports Cars</h2>
+                <Label className = "date-label">Dec 15, 2020</Label>
+              </div>
+                    </Col>
                     
                 </Row>
                 <Row>
-                    <Col md = "12" xs = "12" style = {BlogpageImage4}></Col>
+                    <Col md = "12" xs = "12" style = {BlogpageImage4}>
+                    <div className = "">
+                <Label className = "type-label">New Cars</Label>
+                <h2 className = "car-name">Video: BMW M2 CS is part of Autocar’s 2020 Best Sports Cars</h2>
+                <Label className = "date-label">Dec 15, 2020</Label>
+              </div>
+                    </Col>
                 </Row>
                 
             </Col>
