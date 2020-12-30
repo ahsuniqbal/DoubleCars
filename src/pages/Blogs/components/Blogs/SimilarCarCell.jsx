0d20fa4d09c8b2eb6777629703111cc310e4cefe
abdulrrehman <abdulrrehman45@gmail.com>
2020-12-30 19:29:44 +0500

    fixing

--- src/pages/Blogs/components/Blogs/SimilarCarCell.jsx
+++ src/pages/Blogs/components/Blogs/SimilarCarCell.jsx
@@ -4,12 +4,12 @@ import RelatedStoriesDemoimg from '../../../../assets/RelatedStoriesDemoimg.png'
 import { Row, Col, Card,CardImg,CardTitle, CardSubtitle} from 'reactstrap';
 const SimilarCarCell = () => {
     return(
-        <Card className = "mb-4">
+        <Card className = "mb-4 similar-card-cell">
             <Row className = "">
-                <Col md = "3">
-                    <CardImg className = "img-fluid cell-card-image" src={RelatedStoriesDemoimg} alt="Card image" />
+                <Col md = "5">
+                    <img className = "img-fluid similar-cell-card-image" src={RelatedStoriesDemoimg} alt="Card image" />
                 </Col>
-                <Col md = "9">
+                <Col md = "7" className = "mt-3">
                     <CardTitle className = "similar-car-title">2019 Mercedes Benz Hybrid</CardTitle>
                     <CardSubtitle className="mb-2 similar-card-price">$32,500</CardSubtitle>
                 </Col>
