ae2d69353d1459a3c2c049c8335b0f9f01b3f1f5
abdulrrehman <abdulrrehman45@gmail.com>
2021-01-02 15:16:04 +0500

    working on about

--- src/pages/About/Components/AboutStats.jsx
+++ src/pages/About/Components/AboutStats.jsx
@@ -12,34 +12,38 @@ const AboutStats = () => {
             </Row>
 
             <Row>
-                <Col xs = "6" md = "3">
-                    <Card>
+                <Col xs = "6" md = "3" className = "text-center">
+                    <Card className = "stats-card">
                         <CardBody>
-                            <Label>14,000+</Label>
+                            <Label className = "stats-label">14,000+</Label> <br/>
+                            <Label className = "stats-label2">Cars on sell</Label>
                         </CardBody>
                     </Card>
                 </Col>
 
-                <Col xs = "6" md = "3">
-                <Card>
+                <Col xs = "6" md = "3" className = "text-center">
+                    <Card className = "stats-card">
                         <CardBody>
-                            <Label>1000+</Label>
+                            <Label className = "stats-label">1000+</Label><br/>
+                            <Label className = "stats-label2">Active sellers</Label>
                         </CardBody>
                     </Card>
                 </Col>
 
-                <Col xs = "6" md = "3">
-                <Card>
+                <Col xs = "6" md = "3" className = "text-center">
+                    <Card className = "stats-card">
                         <CardBody>
-                            <Label>5,000+</Label>
+                            <Label className = "stats-label">5,000+</Label><br/>
+                            <Label className = "stats-label2">Cars sold</Label>
                         </CardBody>
                     </Card>
                 </Col>
 
-                <Col xs = "6" md = "3">
-                <Card>
+                <Col xs = "6" md = "3" className = "text-center">
+                    <Card className = "stats-card">
                         <CardBody>
-                            <Label>50,000+</Label>
+                            <Label className = "stats-label">50,000+</Label><br/>
+                            <Label className = "stats-label2">Daily Users</Label>
                         </CardBody>
                     </Card>
                 </Col>
