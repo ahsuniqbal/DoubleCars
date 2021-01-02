3a56f8938f8252ffb90abc1ef90aa3424debb84b
abdulrrehman <abdulrrehman45@gmail.com>
2021-01-02 23:50:14 +0500

    asas

--- src/pages/About/Components/About.jsx
+++ src/pages/About/Components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/About.css'
-import { Row, Col, Container, NavLink} from 'reactstrap';
+import { Row, Col, Container, NavLink, Label} from 'reactstrap';
 import AboutDetails from './AboutDetails';
 import { ChevronRight } from 'react-feather';
 import AboutStats from './AboutStats';
@@ -11,14 +11,12 @@ const About = () => {
         <Row>
           <Col md = "12" xs = "12" sm = "12" className = "aboutus-header">
             <Row>
-              <Col xs = "6" md = "6">
-              <h1 className = "">
-              Making endless posibilities
-            </h1>
-            <h5 className = "">
-            This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in www.doublecars.com. This policy is not applicable to any information collected offline or via channels other than this website. 
-            </h5>
-            <NavLink className = "join-mission-btn" to="">Join Our Mission <ChevronRight className = "about-contact-icon"/></NavLink>
+              <Col xs = "6" md = "4" className = "about-header-content">
+                <h1 className = "about-header-heading">Making endless posibilities</h1>
+                <Label className = "about-header-subheading">
+                  This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in www.doublecars.com. This policy is not applicable to any information collected offline or via channels other than this website. 
+                </Label>
+                <NavLink className = "join-mission-btn" to="">Join Our Mission <ChevronRight className = "about-contact-icon"/></NavLink>
               </Col>
             </Row>
             
