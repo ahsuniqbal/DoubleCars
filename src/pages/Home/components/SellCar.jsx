6d76a75b218d4301d38703264ffbe9bfcf1af5f2
Usama Hussain <usamahussain281999@gmail.com>
2021-01-12 13:32:54 +0500

    home

--- src/pages/Home/components/SellCar.jsx
+++ src/pages/Home/components/SellCar.jsx
@@ -12,58 +12,59 @@ const SellCar = () => {
    
       
     return(
-  
-                <div className='main'>
-                    <div className='first'>
-                        <Row style={{margin:'0'}}>
-                            <Col md='7' sm='6'>
-                               <div className='blue-bg'>
-                                    <h2 className='blue-head'>
-                                            Sell Your Car On Our Platform <br/> With Ease
-                                        </h2>
-                                        <p className='blue-text'> 
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.Impedit laborum blanditiis repellat sapiente harum veniam labore,
-                                            numquam illo provident distinctio voluptatem perferendis possimus est quaerat!
-                                        </p>
-                                        <button className='google-play'><img src={GooglePlayImg} alt='get it on google play'/></button>
-                                        <Button outline className='button'>Learn More</Button>
-                               </div>
+         <div className='main'>
 
-                                <div className='white-bg'>
-                                    <div>
-                                        <img src={Upload}/>
-                                        <h2 className='white-head'>Upload Car</h2>
-                                    
-                                    <p>Download the app and upload your car in few steps</p>
-                                    </div>
-                                    <div>
-                                        <img src={Chat}/>
-                                        <h2 className='white-head'>Get Leads</h2>
-                                    
-                                    <p>Get original leads and keep track on your car seat</p>
-                                    </div>
-                                    <div>
-                                        <img src={Checkbox}/>
-                                        <h2  className='white-head'>Sell Car</h2>
-                                    
-                                    <p>Negotiate with the seller and sell your car!</p>
-                                    </div>
-                                </div>
-
-                            </Col>
-                            <Col  md='5' sm='6'>
-                                <div>
-                                <img src={Mobile}/>
-                                </div>
-
-                                <div></div>
-                            </Col>
-                        </Row>
+            <Row className='blue-bg'>
+                <Col md={8} >
+                    <div className=''>
+                        <h2 className='blue-head'>
+                            Sell Your Car On Our Platform <br/> With Ease
+                        </h2>
+                        <p className='blue-text'> 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.Impedit laborum blanditiis repellat sapiente harum veniam labore,
+                            numquam illo provident distinctio voluptatem perferendis possimus est quaerat!
+                        </p>
+                        <button className='google-play'><img src={GooglePlayImg} alt='get it on google play'/></button>
+                        <Button outline className='button'>Learn More</Button>
                     </div>
-                    <div className='second'>
+                </Col>
+                <Col md={4} >
+                <img src={Mobile}/>
+                </Col>
+            </Row>
 
+            <Row className='white-bg'>
+                <Col md={8} >
+                    <div className=''>
+                        <div>
+                            <div className='d-flex'>
+                                <img src={Upload}/>
+                                <h2 className='white-head'>Upload Car</h2>
+                            </div>
+                            <p className='white-text'>Download the app and upload your car in few steps</p>
+                        </div>
+                        <div>
+                            <div className='d-flex'>
+                                <img src={Chat}/>
+                                <h2 className='white-head'>Get Leads</h2>
+                            </div>
+                            <p className='white-text'>Get original leads and keep track on your car seat</p>
+                        </div>
+                        <div>
+                            <div className='d-flex'>
+                                <img src={Checkbox}/>
+                                <h2  className='white-head'>Sell Car</h2>
+                            </div>
+                            <p className='white-text'>Negotiate with the seller and sell your car!</p>
+                        </div>
                     </div>
-                </div>
+                </Col>
+                <Col md={4} >
+                </Col>
+                
+            </Row>
+            
+        </div>
    
         
     )
@@ -71,3 +72,35 @@ const SellCar = () => {
 }
 
 export default SellCar;
+{/* <div className='blue-bg'>
+<h2 className='blue-head'>
+        Sell Your Car On Our Platform <br/> With Ease
+    </h2>
+    <p className='blue-text'> 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Impedit laborum blanditiis repellat sapiente harum veniam labore,
+        numquam illo provident distinctio voluptatem perferendis possimus est quaerat!
+    </p>
+    <button className='google-play'><img src={GooglePlayImg} alt='get it on google play'/></button>
+    <Button outline className='button'>Learn More</Button>
+</div> */}
+
+// <div className='white-bg'>
+                                    // <div>
+                                    //     <img src={Upload}/>
+                                    //     <h2 className='white-head'>Upload Car</h2>
+                                    
+                                    // <p>Download the app and upload your car in few steps</p>
+                                    // </div>
+                                    // <div>
+                                    //     <img src={Chat}/>
+                                    //     <h2 className='white-head'>Get Leads</h2>
+                                    
+                                    // <p>Get original leads and keep track on your car seat</p>
+                                    // </div>
+                                    // <div>
+                                    //     <img src={Checkbox}/>
+                                    //     <h2  className='white-head'>Sell Car</h2>
+                                    
+                                    // <p>Negotiate with the seller and sell your car!</p>
+                                    // </div>
+//                                 </div>
