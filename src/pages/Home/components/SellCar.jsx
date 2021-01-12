80fdd6207fae0b9428337944ad6316ed91abd72f
Usama Hussain <usamahussain281999@gmail.com>
2021-01-12 16:36:39 +0500

    home

--- src/pages/Home/components/SellCar.jsx
+++ src/pages/Home/components/SellCar.jsx
@@ -6,7 +6,6 @@ import Mobile from '../../../assets/mobile Image.png'
 import Upload from '../../../assets/upload.svg'
 import Chat from '../../../assets/message-square.svg'
 import Checkbox from '../../../assets/check-square.svg'
-import ChatBubbleOutlineRoundedIcon from '@material-ui/icons/ChatBubbleOutlineRounded';
 
 const SellCar = () => {
    
@@ -24,45 +23,54 @@ const SellCar = () => {
                             Lorem ipsum dolor sit amet consectetur adipisicing elit.Impedit laborum blanditiis repellat sapiente harum veniam labore,
                             numquam illo provident distinctio voluptatem perferendis possimus est quaerat!
                         </p>
-                        <button className='google-play'><img src={GooglePlayImg} alt='get it on google play'/></button>
-                        <Button outline className='button'>Learn More</Button>
+                        <button className='google-play m-2'><img src={GooglePlayImg} alt='get it on google play'/></button>
+                        <Button outline className='button m-2'>Learn More</Button>
                     </div>
                 </Col>
                 <Col md={4} >
-                <img src={Mobile}/>
+              
                 </Col>
             </Row>
-
+            
             <Row className='white-bg'>
                 <Col md={8} >
                     <div className=''>
-                        <div>
-                            <div className='d-flex'>
-                                <img src={Upload}/>
-                                <h2 className='white-head'>Upload Car</h2>
+                        <div className='row d-flex'>
+                            <div className='col-lg-4 md-12'>
+                                <div className='white-main'>
+                                    <div className='d-flex'>
+                                        <img src={Upload} className='white-section-icon'/>
+                                        <h2 className='white-head ml-5'>Upload Car</h2>
+                                    </div>
+                                    <p className='white-text pt-2'>Download the app and upload your car in few steps</p>
+                                </div>
                             </div>
-                            <p className='white-text'>Download the app and upload your car in few steps</p>
-                        </div>
-                        <div>
-                            <div className='d-flex'>
-                                <img src={Chat}/>
-                                <h2 className='white-head'>Get Leads</h2>
+                            <div className='col-lg-4 md-12'>
+                                <div  className='white-main'>
+                                    <div className='d-flex'>
+                                        <img src={Chat} className='white-section-icon'/>
+                                        <h2 className='white-head  ml-5'>Get Leads</h2>
+                                    </div>
+                                    <p className='white-text pt-2'>Get original leads and keep track on your car seat</p>
+                                </div>
                             </div>
-                            <p className='white-text'>Get original leads and keep track on your car seat</p>
-                        </div>
-                        <div>
-                            <div className='d-flex'>
-                                <img src={Checkbox}/>
-                                <h2  className='white-head'>Sell Car</h2>
+                            <div className='col-lg-4 md-12'>
+                                <div className='white-main2'>
+                                    <div className='d-flex'>
+                                        <img src={Checkbox} className='white-section-icon'/>
+                                        <h2  className='white-head  ml-5'>Sell Car</h2>
+                                    </div>
+                                    <p className='white-text pt-2'>Negotiate with the seller and sell your car!</p>
+                                </div>
                             </div>
-                            <p className='white-text'>Negotiate with the seller and sell your car!</p>
                         </div>
                     </div>
                 </Col>
                 <Col md={4} >
                 </Col>
-                
+                <img src={Mobile} className='mobile-img'/>
             </Row>
+            
             
         </div>
    
