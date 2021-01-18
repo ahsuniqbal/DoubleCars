d211767c918712edfa31541127b89ebbc6975bb0
abdulrrehman <abdulrrehman45@gmail.com>
2021-01-18 13:03:49 +0500

    working on about

--- src/pages/About/components/AboutChoose.jsx
+++ src/pages/About/components/AboutChoose.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Row, Label, Button } from 'reactstrap';
+import AboutCheck from '../../../assets/AboutCheck.svg'
 import AboutChooseDummy from '../../../assets/AboutChooseDummy.png'
 import '../styles/AboutChoose.css'
 import { Check} from 'react-feather';
@@ -12,20 +13,20 @@ const AboutChoose = () => {
            </Col>
            <Col md = "1" className = "about-choose-col"></Col>
            <Col xs = "12" md = "7" className = "about-choose-col">
-                <h2>Why choose us?</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci lorem, mattis at porta vitae, accumsan vel libero. In sit amet magna nunc. Curabitur at purus iaculis, rutrum neque sit amet.</p>
+                <h2 className = "choose-heading">Why choose us?</h2>
+                <p className = "choose-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci lorem, mattis at porta vitae, accumsan vel libero. In sit amet magna nunc. Curabitur at purus iaculis, rutrum neque sit amet.</p>
                 <ul className="choose-list">
-                        <li className="nav-item">
-                        <p><Check className = "about-check-icon"/>  Lorem ipsum dolor sit amet consectetur.</p>
+                        <li className="">
+                        <p><img src = {AboutCheck} className = "img-fluid about-check-icon"/>Lorem ipsum dolor sit amet consectetur.</p>
                         </li>
-                        <li className="nav-item">
-                        <p><Check className = "about-check-icon"/> Lorem ipsum dolor sit amet consectetur.</p>
+                        <li className="">
+                        <p><img src = {AboutCheck} className = "img-fluid about-check-icon"/>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         </li>
-                        <li className="nav-item">
-                        <p><Check className = "about-check-icon"/> Lorem ipsum dolor sit amet consectetur.</p>
+                        <li className="">
+                        <p><img src = {AboutCheck} className = "img-fluid about-check-icon"/>Lorem ipsum dolor sit ametDonec orci lorem, mattis at porta vitae, accumsan vel libero. </p>
                         </li>
-                        <li className="nav-item">
-                            <p><Check className = "about-check-icon"/> Lorem ipsum dolor sit amet consectetur.</p>
+                        <li className="">
+                            <p><img src = {AboutCheck} className = "img-fluid about-check-icon"/>Lorem ipsum dolor sit ametDonec orci lorem, mattis at porta.</p>
                         </li>
                        
                         
