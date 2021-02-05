229bd0b6c0a919c1ebb79b438fb9ea1aea9c6367
ahsuniqbal <ahsuniqbal1505@gmail.com>
2021-02-06 00:09:55 +0500

    connection string changed

--- src/config/ConnectionString.jsx
+++ src/config/ConnectionString.jsx
@@ -4,7 +4,7 @@
 // const connectionString = "https://doublecars.herokuapp.com/"
 
 // Google compute base url connection string
-const connectionString = "https://doublecars.herokuapp.com/";
+const connectionString = "https://doublecarsnode.herokuapp.com/";
 // const connectionString = "http://localhost:3002/";
 
 // Vinaudit base url
