7279c17685eeed6002fd1c1cfea9ca4316fa5ba6
ahsuniqbal <ahsuniqbal1505@gmail.com>
2021-03-23 19:32:03 +0500

    save icons

--- src/components/ProductCard/api/GetRequest.jsx
+++ src/components/ProductCard/api/GetRequest.jsx
@@ -1,18 +1,18 @@
 import { connectionString } from '../../../config/ConnectionString';
 const axios = require('axios');
 
-export const GetIfSaved = (productId, userId) => {
-    return new Promise((resolve, reject) => {
-        var url = connectionString + "products/" + productId + "?id=" + userId;
-        axios.get(url).then(function(response){
-            const data = response.data.results;
+// export const GetIfSaved = (productId, userId) => {
+//     return new Promise((resolve, reject) => {
+//         var url = connectionString + "products/" + productId + "?id=" + userId;
+//         axios.get(url).then(function(response){
+//             const data = response.data.results;
             
-            resolve(data);
-        })
-        .catch(function(error){
-            let updatedData = false;
-            resolve(updatedData);
-            console.log("Error: ", error);
-        })
-    })
-};
+//             resolve(data);
+//         })
+//         .catch(function(error){
+//             let updatedData = false;
+//             resolve(updatedData);
+//             console.log("Error: ", error);
+//         })
+//     })
+// };
