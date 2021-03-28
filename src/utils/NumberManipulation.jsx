c83c7744154561bd1efc67470c4630d94559e207
ahsuniqbal <ahsuniqbal1505@gmail.com>
2021-03-28 11:30:58 +0500

    try catch added in number manipulation

--- src/utils/NumberManipulation.jsx
+++ src/utils/NumberManipulation.jsx
@@ -1,4 +1,9 @@
 // Add commas to a number
 export const AddCommaToNumber = (number) => {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    try {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    catch(error) {
+        console.error("Invalid format", error.message);
+    }
 }
