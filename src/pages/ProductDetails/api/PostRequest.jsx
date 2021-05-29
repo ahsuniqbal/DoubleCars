afa2d431330f99bda0d35700af7272bb9fa34c5e
ashar312 <ashar.ashfaq2@gmail.com>
2021-05-29 17:57:57 +0500

    small email changes.

--- src/pages/ProductDetails/api/PostRequest.jsx
+++ src/pages/ProductDetails/api/PostRequest.jsx
@@ -19,3 +19,22 @@ export const SendEmail = (obj) => {
         })
       })   
 }
+
+export const messageChatMail = (obj) => {
+    return new Promise((resolve, reject) => {
+        var url = connectionString + "users/chat-message-mail"
+        axios.post(url,obj)
+        .then(function (response) {
+            // handle success
+            const data = response.data.results;
+            let updatedData = data
+            resolve(updatedData)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error)
+            let updatedData = false
+            resolve(updatedData)
+        })
+      })   
+}
