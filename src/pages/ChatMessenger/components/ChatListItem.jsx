90438f3d8e9681122cd6741b05416eab743f2c6d
ashar312 <ashar.ashfaq2@gmail.com>
2021-06-06 16:45:04 +0500

    working.

--- src/pages/ChatMessenger/components/ChatListItem.jsx
+++ src/pages/ChatMessenger/components/ChatListItem.jsx
@@ -4,15 +4,42 @@ import { connect } from 'react-redux';
 import { selectChat } from '../../../redux/actions/ChatActions.jsx';
 import dummyAvatar from '../../../assets/dummyAvatar.jpg'
 import '../styles/ChatListItem.css';
-
+import {postReadCount} from '../../../components/Firebase/database'
+const firebase = require('firebase').default
 
 const mapDispatchToProps = (dispatch) => {
     return {
         selectChat: (chat) => {
             console.log('Hello Ashar chat',chat)
+            readHasCountFalseFunc(chat.chat)
             dispatch(selectChat(chat));
         }
     }
+}
+
+const readHasCountFalseFunc = (chat) => {
+    const userId = localStorage.getItem('userId');
+    var obj = {}
+    
+    if(chat.receiverId == userId){
+        obj = {
+            receiverUnreadCount : 0,
+            receiverHasRead : true
+        }
+    }else{
+        obj = {
+            senderUnreadCount : 0,
+            senderHasRead : true
+        }
+    }
+    console.log('AsharObj',obj)
+    postReadCount(chat.senderId, chat.receiverId,obj)
+    .then(doc => {
+        console.log('asharDoc',doc)
+    }).catch(error => {
+        console.log('AsharError',error)
+    })
+
 }
 
 
@@ -45,9 +72,15 @@ const ChatListItem = (props) => {
     const getNotify = (chat) => {
         var userId = localStorage.getItem('userId')
         if(chat.receiverId == userId){
-            return chat.receiverHasRead
+            return {
+                status : !chat.receiverHasRead,
+                count : chat.senderUnreadCount
+            }
         }else if(chat.senderId == userId){
-            return chat.senderHasRead
+            return {
+                status : !chat.senderHasRead,
+                count : chat.receiverUnreadCount
+            }
         }
     }
 
@@ -111,7 +144,7 @@ const ChatListItem = (props) => {
             <Col xs="3" className="text-center">
                 <Label className="time">{lastMsgAt(props.chat.chat.lastMessageAt)}</Label>
                 {
-                    getNotify(props.chat.chat) ? <div className="unread-number">1</div> : null
+                    getNotify(props.chat.chat).status ? <div className="unread-number">{getNotify(props.chat.chat).count}</div> : null
                 }
                 
             </Col>
