ae2a8a8fb8b7fdd7a54cefad4e5411995c9dd9e4
touseef-haider <touseefhaider355@gmail.com>
2021-06-07 18:40:50 +0500

    done with the resp

--- src/pages/Home/components/Searchbar.jsx
+++ src/pages/Home/components/Searchbar.jsx
@@ -90,7 +90,7 @@ const Searchbar = () => {
                             
                         </Col>
                        
-                            <Col xs="12" sm="6" md="2" className='m-1' >
+                            <Col xs="12" sm="6" md="2" className='search-bar-padding my-1' >
                                 {/* <Input id="make-list" type="select" className = "condition-dropdown">
                                     <option value="" hidden selected>Condition</option>
                                     <option value={false}>New</option>
