bd11e55d5efb62cae4aee9f04d1d2bda22a7b7a5
touseef-haider <touseefhaider355@gmail.com>
2021-06-09 20:29:55 +0500

    done with the responsiveness

--- src/pages/ProductDetails/components/Gallery.jsx
+++ src/pages/ProductDetails/components/Gallery.jsx
@@ -104,7 +104,7 @@ const Gallery = (props) => {
                     showPlayButton={false}
                     showThumbnails={matched}
                     showNav={matched}
-                    autoPlay={matched ? true : false}
+                    autoPlay={matched}
                     showFullscreenButton={matched}
                 />
             </div>
