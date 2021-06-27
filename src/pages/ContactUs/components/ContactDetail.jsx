8748d975a49fdf859e29c42d8ad8c9e9760bca3b
ASHAR ASHFAQ <ashar.ashfaq@m3tech.com.pk>
2021-06-27 20:28:12 +0500

    contact image

--- src/pages/ContactUs/components/ContactDetail.jsx
+++ src/pages/ContactUs/components/ContactDetail.jsx
@@ -11,38 +11,38 @@ const ContactDetail = () => {
             <Row className = "d-flex justify-content-center">
                 <Col xs = "12" md = "3" className = "lower-outer-col">
                     <Row>
-                        <Col className = "contact-icon-column"  xs = "6" md = "3">
+                        <Col className = "contact-icon-column icons-row-mobile"  xs = "12" md = "3">
                             <img src = {ContactMessage} alt = "message-icon"/>
                         </Col>
-                        <Col  xs = "6" md = "9">
+                        <Col  xs = "12" md = "9" className = "icons-row-mobile">
                             <Label className = "contact-label-head">Email Address</Label> <br/>
                             <Label className = "contact-detail">Contact@doublecars.com</Label>
                         </Col>
                     </Row>
                 </Col>
-                <Col md = "1" className = "lower-outer-col">
+                <Col md = "1" className = "lower-outer-col v-line">
                     <div className = "vertical-line"></div>
                 </Col>
                 <Col xs = "12" md = "3" className = "lower-outer-col">
                     <Row>
-                        <Col  xs = "6" md = "3" className = "contact-icon-column">
+                        <Col  xs = "12" md = "3" className = "contact-icon-column icons-row-mobile">
                             <img  src = {ContactPhone} alt = "Phone-icon"/>
                         </Col>
-                        <Col  xs = "6" md = "9">
+                        <Col  xs = "12" md = "9" className = "icons-row-mobile">
                             <Label className = "contact-label-head">Phone Number</Label> <br/>
                             <Label className = "contact-detail">+1 2634 546487</Label>
                         </Col>
                     </Row>
                 </Col>
-                <Col md = "1" className = "lower-outer-col">
+                <Col md = "1" className = "lower-outer-col v-line">
                     <div className = "vertical-line"></div>
                 </Col>
                 <Col xs = "12" md = "3" className = "lower-outer-col">
                     <Row>
-                        <Col className = "contact-icon-column" xs = "6" md = "3">
+                        <Col className = "contact-icon-column icons-row-mobile" xs = "12" md = "3">
                             <img src = {ContactLocation} alt = "message-icon"/>
                         </Col>
-                        <Col  xs = "6" md = "9" >
+                        <Col  xs = "12" md = "9" className = "icons-row-mobile">
                             <Label className = "contact-label-head">Office</Label> <br/>
                             <Label className = "contact-detail">145 AB, Santa Ana, CA</Label>
                         </Col>
